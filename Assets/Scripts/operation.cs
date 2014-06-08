24b457f3fecfd4ea809d2ae7e56700bfc185bdde
U-Tkingless-PC\Tkingless <Tkingless@Tkingless-PC.(none)>
2014-06-09 00:27:47 +0800

    third commit

---
+++ Assets/Scripts/operation.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class operation : MonoBehaviour {
+
+	//transform of a segment
+	public Transform[] mSegments;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+public class Disk{
+
+
+}
+
+public class DiskSegment {
+
+	Transform mTransform;
+	Vector2 mCoordinate; //(r,theta)
+
+	public DiskSegment (Transform aTrans, Vector2 aCoor){
+
+		if (aTrans != null)
+			mTransform = aTrans;
+		else
+			Debug.LogException(
+	}
+}
