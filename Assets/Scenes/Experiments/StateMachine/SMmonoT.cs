a07f1051214bd48f69a34b915616194a7ab5e6ce
tkingless <unstashworld@gmail.com>
2014-06-17 18:11:34 +0800

    tuesday

---
+++ Assets/Scenes/Experiments/StateMachine/SMmonoT.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SMmonoT : MonoBehaviour {
+
+	//state machine monobehavior test
+
+	public enum State {
+		MoveUp,
+		MoveDown,
+		MoveLeft,
+		MoveRight
+	}
+
+	Util.Mode<SMmonoT,State> mMode; //this state machine has a limitation that callback fucntion limited to void()
+
+	void Start () {
+
+		mMode = new Util.Mode<SMmonoT, State>(this);
+		mMode.Set (State.MoveUp);
+	}
+
+	void Update()
+	{
+		mMode.Proc();
+	}
+
+	float Move_counter;
+	void MoveUp_Init () {
+		Move_counter = 0f;
+	}
+
+	void MoveUp_Proc () {
+
+		Move_counter += Time.deltaTime;
+		transform.Translate (Vector3.up*Time.deltaTime * 10.0f);
+
+		//by switch to see the usage:
+#if true 
+		if (Move_counter > 1){
+			mMode.Term(); //it will call mMode.mCurr 's Term() and then call mMode.mCurr's init() to start over
+		}
+#endif
+
+		if (Move_counter > 2)
+						mMode.Set (State.MoveDown);
+	}
+
+	void MoveUp_Term () {
+
+		Debug.Log ("MoveUp_Term() is called");
+		mMode.Set (State.MoveDown);
+		}
+
+	void MoveDown_Proc () {
+
+		Move_counter += Time.deltaTime;
+		transform.Translate (Vector3.down*Time.deltaTime * 10.0f);
+		if (Move_counter > 4)
+			mMode.Set (State.MoveUp);
+	}
+
+	void MoveDown_Term () {
+		
+		Debug.Log ("MoveDown_Term() is called");
+	}
+}
---
+++ Assets/Scenes/Experiments/StateMachine/UtilMode.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+//using System.Reflection;
+
+namespace Util
+{
+	class Mode<T, ID>
+	{
+		T mObj;
+		Type mModeIDType;
+		
+		delegate void ModeFunc();
+		
+		ModeFunc[] mInitFunc;
+		ModeFunc[] mProcFunc;
+		ModeFunc[] mTermFunc;
+		
+		int mPrev = -1;
+		int mCurr = -1;
+		
+		public Mode(T ob)
+		{
+			mObj = ob;
+			mModeIDType = typeof(ID);
+			SetUp();
+			mCurr = 0;
+		}
+		
+		void SetUp()
+		{
+			if (mModeIDType.IsEnum)
+			{
+				Array ary = Enum.GetValues(mModeIDType);
+				int len = ary.Length;
+				mInitFunc = new ModeFunc[len];
+				mProcFunc = new ModeFunc[len];
+				mTermFunc = new ModeFunc[len];
+				
+				foreach (int enumVal in ary)
+				{
+					string enumName =  Enum.GetName(mModeIDType, enumVal);
+					mInitFunc[enumVal] = (ModeFunc) Delegate.CreateDelegate(typeof(ModeFunc), mObj, enumName + "_Init", false, false);
+					mProcFunc[enumVal] = (ModeFunc) Delegate.CreateDelegate(typeof(ModeFunc), mObj, enumName + "_Proc", false, false);
+					mTermFunc[enumVal] = (ModeFunc) Delegate.CreateDelegate(typeof(ModeFunc), mObj, enumName + "_Term", false, false);
+				}
+			}
+			
+		}
+		
+		public void Init(Enum modeID)
+		{
+			Set(modeID);
+			init();
+		}
+		
+		public void Proc()
+		{
+			while(mPrev != mCurr)
+			{
+				term();
+				init();
+			}
+			proc();
+		}
+		
+		public void Term()
+		{
+			term();
+		}
+		
+		
+		void init()
+		{
+			if (mCurr != -1)
+			{
+				mPrev = mCurr;
+				if(mInitFunc[mCurr] != null) mInitFunc[mCurr]();
+			}
+		}
+		void proc()
+		{
+			if(mProcFunc[mCurr] != null) mProcFunc[mCurr]();
+		}
+		void term()
+		{
+			if (mPrev != -1)
+			{
+				if(mTermFunc[mPrev] != null) mTermFunc[mPrev]();
+			}
+			mPrev = -1;
+		}
+		
+		public static Mode<T, ID> operator ++(Mode<T, ID> mode)
+		{
+			++mode.mCurr;
+			return mode;
+		}
+		
+		public Mode<T, ID> Set(Enum modeID)
+		{
+			mCurr = (int)(ValueType)modeID;
+			return this;
+		}
+		
+		public int Get()
+		{
+			return mCurr;
+		}
+	}
+}
