05a781b38177b36e5299460a5923b2f5f5453e25
tkingkwun <kingkwun.tsang@nxtomo.hk>
2014-11-06 10:49:31 +0800

    add back the utility script

---
+++ Unity/Assets/Scripts/DiskUtility.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DiskUtility {
+
+	public static int GetConjugateTheta (int i, int ThetaMod)
+	{
+		i += ThetaMod / 2;
+		if (i > ThetaMod)
+			i -= ThetaMod;
+		return i;
+	}
+
+	public static int GetDegreeTheta (int i, int ThetaMod)
+	{
+		int degree = 0;
+		degree = i * 360 / ThetaMod;
+		if (i == ThetaMod)
+						degree = 0;
+		return degree;
+		}
+
+}
