139eed91c3927384acad4472c7ad80e477ef81d1
tkingkwun <kingkwun.tsang@nxtomo.hk>
2014-11-17 13:10:58 +0800

    update php2

---
+++ Php/RankingDownload.php
@@ -0,0 +1,30 @@
+<?php
+//主機位址
+$dbhost = 'localhost';
+//資料庫名稱
+$dbname = 'highscore';
+//資料表名稱
+$dbtablename = 'ranking';
+//資料庫使用者名稱
+$dbuser = 'root';
+
+//MySQL 連接
+mysql_connect($dbhost,$dbuser) or die(mysql_error());
+//資料庫連接
+mysql_select_db($dbname) or die(mysql_error());
+
+if( $_POST['download'] != NULL ) {
+
+	$query = "select * from $dbtablename order by score desc";
+	$result = mysql_query($query) or die(mysql_error());
+	$rows = mysql_num_rows($result);
+
+	for($cnt = 0; $cnt < $rows; $cnt++) {
+
+		$row = mysql_fetch_array($result);
+		echo "Name = " . $row[1];
+		echo "Score = " . $row[2];
+	}
+}
+
+?>
---
+++ Unity/Assets/Scenes/Experiments/Networking/UI.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UI : MonoBehaviour {
+
+	public string path = "http://localhost/Unity/RandingUpdate.php";
+	public string downloadPath = "http://localhost/Unity/RankingDownload.php";
+
+	public string name;
+	public string score;
+
+	void OnGUI () {
+
+		name = GUILayout.TextField(name,10);
+		score = GUILayout.TextField(score,10);
+
+		if(GUILayout.Button("Update") ){
+			StartCoroutine("ScoreUpdate");
+		}
+	}
+
+	public IEnumerator ScoreUpdate() {
+
+		WWWForm form = new WWWForm();
+
+		Dictionary<string,string> data = new Dictionary<string, string>();
+		data.Add("name",name);
+		data.Add("score", score);
+
+		foreach( KeyValuePair<string,string> post in data) {
+
+			form.AddField(post.Key, post.Value);
+		}
+
+		WWW www = new WWW(path,form);
+
+		yield return www;
+
+		Debug.Log(www.text);
+	}
+
+	public IEnumerator ScoreDownload() {
+
+		WWWForm form = new WWWForm();
+
+		Dictionary<string,string> data = new Dictionary<string, string>();
+		data.Add("download","1");
+
+		foreach( KeyValuePair<string,string> post in data) {
+			form.AddField(post.Key, post.Value);
+		}
+
+		WWW www = new WWW(downloadPath, form);
+
+		yield return www;
+
+		Debug.Log(www.text);
+	}
+}
