c74c8c336aa01a6bb00186764ef29212138c843c
tkingkwun <kingkwun.tsang@nxtomo.hk>
2014-11-17 11:57:44 +0800

    update php

--- Php/RankingUpdate.php
+++ Php/RankingUpdate.php
@@ -13,4 +13,15 @@ mysql_connect( $dbhost, $dbuser) or die(mysql_error());
 //資料庫連接
 mysql_select_db($dbname) or die(mysql_error());
 
-if( $_POST['name']) != NULL &&
+if( $_POST['name']) != NULL && $_POST['score'] != NULL) {
+
+	//echo 可以在Unity 中的 Console 面板顯現
+	echo 'Ranking Update';
+
+	$name = $_POST['name'];
+	$score	= (int)$_POST['score'];
+
+	$query = "insert into $dbtablename(id, name, score) values (null, '$name', '$score')";
+	$result = mysql_query($query) or die(mysql_error());
+}
+?>
